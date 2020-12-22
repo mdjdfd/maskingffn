eccc7293400e16d6873608f808d036bf61b11071
mdjdfd <fahadcse11@gmail.com>
2020-12-22 14:54:18 +0100

    Masking on first layer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from deepstruct.learning import train
 from helper import get_mnist_loaders
 
 
-def random_masking_output_layer():
+def random_masking_first_layer():
     batch_size = 10
     train_loader, test_loader = get_mnist_loaders(batch_size)
 
@@ -27,24 +27,23 @@ def random_masking_output_layer():
 
     for epoch in range(epochs):
         train(train_loader, model, optimizer, loss, device)
-    assert run_evaluation(test_loader, model, device) > 1 / output_size
 
     model.apply_mask()
     model.recompute_mask(theta=0.01)
 
-    n = 10
-    d = 10
+    n = 100
+    d = 784
     random_mat = torch.rand(n, d)
     k = round(0.50 * d)
     k_th_quantile = torch.topk(random_mat, k, largest=False)[0][:, -1:]
     bool_tensor = random_mat <= k_th_quantile
     random_mask = torch.where(bool_tensor, torch.tensor(1), torch.tensor(0))
 
-    output_layer_mask = model.layer_out.get_mask()
+    first_layer_mask = model.layer_first.get_mask()
 
-    result_mask = random_mask * output_layer_mask
+    result_mask = random_mask * first_layer_mask
 
     return result_mask
 
 
-print(random_masking_output_layer())
+print(random_masking_first_layer())
