bc760c6a00788d3d39860bb638fa31866ba7ac4b
mdjdfd <fahadcse11@gmail.com>
2021-07-13 11:43:19 +0200

    passing mask as a module

--- launcher.py
+++ launcher.py
@@ -4,15 +4,21 @@ import visualization as vis
 import torch
 import os
 import helper as hp
-import wticket_retraining as ret
+import wticket_retraining
 import parameter as prm
+import pickle
 
 
 def main():
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
     # prm.store_param()
-    ret.retrain_wticket()
+
+    wticket_mask_path = "storage/2021-07-11-111745-fe392342-d71c-4ee8-a1fe-712f13e9a884/0/lt_mask_90.0.pkl"
+    mask = []
+    with open(wticket_mask_path, 'rb') as f:
+        mask.append(pickle.load(f))
+    wticket_retraining.Retrain(mask)
 
     # test_accuracy, model = tr.config_and_train()
     # pr.network_prune()
--- wticket_retraining.py
+++ wticket_retraining.py
@@ -5,16 +5,15 @@ import torch.nn as nn
 import deepstruct.sparse
 import pickle
 import utils
-import numpy as np
+
 from tqdm import tqdm
 from deepstruct.learning import train
 from deepstruct.learning import run_evaluation
 
 
-
 def retrain_wticket():
     wticket_model_path = "storage/2021-07-11-111745-fe392342-d71c-4ee8-a1fe-712f13e9a884/0/lt_train_epoch_37.pt"
-    wticket_mask_path = "storage/2021-07-11-111745-fe392342-d71c-4ee8-a1fe-712f13e9a884/0/lt_mask_90.0.pkl"
+    # wticket_mask_path = "storage/2021-07-11-111745-fe392342-d71c-4ee8-a1fe-712f13e9a884/0/lt_mask_90.0.pkl"
 
     batch_size = 10
     train_loader, test_loader = hp.get_mnist_loaders(batch_size)
@@ -28,11 +27,11 @@ def retrain_wticket():
 
     model.load_state_dict(torch.load(wticket_model_path))
 
-    mask = []
-    with open(wticket_mask_path, 'rb') as f:
-        mask.append(pickle.load(f))
+    # mask = []
+    # with open(wticket_mask_path, 'rb') as f:
+    #     mask.append(pickle.load(f))
 
-    model.apply_mask(mask)
+    model.apply_mask()
     # weights = model.state_dict()
     #
     # layers = list(model.state_dict())
@@ -46,22 +45,19 @@ def retrain_wticket():
     loss = nn.CrossEntropyLoss()
 
     training_epochs = 50
-    do_retraining(train_loader, test_loader, model, optimizer, loss, device, training_epochs)
 
-
-def do_retraining(train_loader, test_loader, wticket_model, optimizer, loss, device, training_epochs):
-    train_loss_arr = np.zeros(training_epochs, float)
-    test_accuracy_arr = np.zeros(training_epochs, float)
-
-    utils.print_nonzeros(wticket_model)
+    utils.print_nonzeros(model)
     progress_bar = tqdm(range(training_epochs))
 
     for train_epoch in progress_bar:
-        accuracy = run_evaluation(test_loader, wticket_model, device)
-        test_accuracy_arr[train_epoch] = accuracy
+        accuracy = run_evaluation(test_loader, model, device)
 
-        train_loss, train_accuracy = train(train_loader, wticket_model, optimizer, loss, device)
-        train_loss_arr[train_epoch] = train_loss
+        train_loss, train_accuracy = train(train_loader, model, optimizer, loss, device)
 
         progress_bar.set_description(
             f'Train Epoch: {train_epoch + 1}/{training_epochs} Loss: {train_loss:.6f} Accuracy: {accuracy:.2f}%')
+
+
+class Retrain:
+    def __init__(self):
+        super(Retrain, self).__init__()
