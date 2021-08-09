031692ad61900d1ef6be4cbb9dca2c022bcf7d8e
mdjdfd <fahadcse11@gmail.com>
2021-08-09 15:52:14 +0200

    pruning 90%

--- wticket.py
+++ wticket.py
@@ -66,7 +66,7 @@ def run_model(storage_path):
 
     # Start of Pruning Functionality
     # best_accuracy = 0
-    prune_percentile = 50
+    prune_percentile = 90
     ITERATION = 10
     training_epochs = 50
 
